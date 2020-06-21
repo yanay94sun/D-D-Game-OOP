b6e32e48dba18c1370f8851e145eae5348ffa9f8
Yanay <yanayshem@gmail.com>
2020-06-21 18:59:00 +0300

    Unit

---
+++ src/Unit.java
@@ -0,0 +1,9 @@
+public abstract class Unit extends Tile {
+    private String name;
+    private int healthPool;
+    private int healthAmount;
+    private int attackPoints;
+    private int defensePoints;
+    public abstract String getDescription();
+
+}
