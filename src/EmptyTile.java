7202d719354e040dc7056292a5a224a463f7ab9b
refael-farjun <refaelfarjun5@gmail.com>
2020-06-22 06:21:21 +0300

    classes WallTile and EmptyTile are FINISHED

---
+++ src/EmptyTile.java
@@ -0,0 +1,8 @@
+public class EmptyTile extends Tile {
+
+    public EmptyTile(int x, int y){
+        this.tile = '.';
+        this.coordinate = new Coordinate(x, y);
+    }
+
+}
---
+++ src/WallTile.java
@@ -0,0 +1,7 @@
+public class WallTile extends Tile {
+
+    public WallTile(int x, int y){
+        this.tile = '#';
+        this.coordinate = new Coordinate(x, y);
+    }
+}
